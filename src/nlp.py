e61ff18c551569ad33e9c370ec9661e8c491c28d
Kevin Liao <lwk723@berkeley.edu>
2018-12-02 14:37:34 -0800

    fix nlp

--- src/nlp.py
+++ src/nlp.py
@@ -32,20 +32,20 @@ def decontracted(text):
     de-contract the contraction
     """
     # specific
-    text = re.sub(r"(W|w)on\'t", "will not", text)
-    text = re.sub(r"(C|c)an\'t", "can not", text)
-    text = re.sub(r"(Y|y)\'all", "you all", text)
+    text = re.sub(r"(W|w)on(\'|\’)t", "will not", text)
+    text = re.sub(r"(C|c)an(\'|\’)t", "can not", text)
+    text = re.sub(r"(Y|y)(\'|\’)all", "you all", text)
 
     # general
-    text = re.sub(r"(I|i)\'m", "i am", text)
-    text = re.sub(r"(A|a)in\'t", "is not", text)
-    text = re.sub(r"n\'t", " not", text)
-    text = re.sub(r"\'re", " are", text)
-    text = re.sub(r"\'s", " is", text)
-    text = re.sub(r"\'d", " would", text)
-    text = re.sub(r"\'ll", " will", text)
-    text = re.sub(r"\'t", " not", text)
-    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"(I|i)(\'|\’)m", "i am", text)
+    text = re.sub(r"(A|a)in(\'|\’)t", "is not", text)
+    text = re.sub(r"n(\'|\’)t", " not", text)
+    text = re.sub(r"(\'|\’)re", " are", text)
+    text = re.sub(r"(\'|\’)s", " is", text)
+    text = re.sub(r"(\'|\’)d", " would", text)
+    text = re.sub(r"(\'|\’)ll", " will", text)
+    text = re.sub(r"(\'|\’)t", " not", text)
+    text = re.sub(r"(\'|\’)ve", " have", text)
     return text
 
 
@@ -53,7 +53,7 @@ def spacing_punctuation(text):
     """
     add space before and after punctuation and symbols
     """
-    re_tok = re.compile(f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤$&#‘’-])')
+    re_tok = re.compile(f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤$&#‘’''-])')
     return re_tok.sub(r' \1 ', text)
 
 
